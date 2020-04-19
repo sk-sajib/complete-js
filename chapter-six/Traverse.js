5d53bd86c9c86c3bb07ee4b87857a0d207c3cd36
sk-sajib <sksajib0000@gmail.com>
2020-04-19 22:16:40 +0600

    done

--- chapter-six/Traverse.js
+++ chapter-six/Traverse.js
@@ -1,7 +1,7 @@
 // Traverse 
 
 
-var arr = [4,5,6,7,8]
+//var arr = [4,5,6,7,8]
 
 // for (var i=0;i<arr.length;i++){
 //    // console.log(arr[i])
@@ -17,14 +17,63 @@ var arr = [4,5,6,7,8]
 // console.log(sum)
 
 
-for( var i =0;i<arr.length;i++){
-    if(arr[i] % 2 == 0){
-        console.log(arr[i])
-    }
+// for( var i =0;i<arr.length;i++){
+//     if(arr[i] % 2 == 0){
+//         console.log(arr[i])
+//     }
+// }
+
+// for(var i=0;i<arr.length;i++){
+//     if(arr[i]%2 == 0){
+//       console.log(arr[i])  
+//     }
+// }
+
+
+//var arr = [1,2,3,4,5]
+
+// for (var i = 0; i<arr.length;i++){
+//     //console.log(arr[i])
+//     arr[i] = arr[i] + 2
+// }
+// console.log(arr)
+
+// var sum = 0;
+
+// for (var i =0; i<arr.length;i++){
+//     sum = sum + arr[i]
+// }
+// console.log(sum)
+
+// for (var i =0;i<arr.length;i++){
+//     if(arr[i]%2 == 0){
+//         console.log(arr[i])
+//     } 
+// }
+
+
+var arr = [1,2,3,5,7,88,63]
+
+var sum_even = 0
+
+var sum_odd = 0
+
+for(var i =0;i<arr.length;i++){
+
+if(arr[i]%2===0){
+
+sum_even=sum_even+arr[i]
+
+
+
+}else if (arr[i]%2===1) {
+
+sum_odd=sum_odd+arr[i]
+
 }
 
-for(var i=0;i<arr.length;i++){
-    if(arr[i]%2 == 0){
-      console.log(arr[i])  
-    }
 }
+
+console.log("Sum of Even:"+sum_even)
+
+console.log("sum of Odd:"+sum_odd)
