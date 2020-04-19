3d680d3665861a94b341fce66365aa4297f2ff68
sk-sajib <sksajib0000@gmail.com>
2020-04-19 22:23:44 +0600

    insert-remove.js added

--- chapter-six/Traverse.js
+++ chapter-six/Traverse.js
@@ -30,7 +30,7 @@
 // }
 
 
-//var arr = [1,2,3,4,5]
+var arr = [1,2,3,4,5]
 
 // for (var i = 0; i<arr.length;i++){
 //     //console.log(arr[i])
@@ -48,32 +48,32 @@
 // for (var i =0;i<arr.length;i++){
 //     if(arr[i]%2 == 0){
 //         console.log(arr[i])
-//     } 
+//     }
 // }
 
 
-var arr = [1,2,3,5,7,88,63]
+// var arr = [1,2,3,5,7,88,63]
 
-var sum_even = 0
+// var sum_even = 0
 
-var sum_odd = 0
+// var sum_odd = 0
 
-for(var i =0;i<arr.length;i++){
+// for(var i =0;i<arr.length;i++){
 
-if(arr[i]%2===0){
+// if(arr[i]%2===0){
 
-sum_even=sum_even+arr[i]
+// sum_even=sum_even+arr[i]
 
 
 
-}else if (arr[i]%2===1) {
+// }else if (arr[i]%2===1) {
 
-sum_odd=sum_odd+arr[i]
+// sum_odd=sum_odd+arr[i]
 
-}
+// }
 
-}
+// }
 
-console.log("Sum of Even:"+sum_even)
+// console.log("Sum of Even:"+sum_even)
 
-console.log("sum of Odd:"+sum_odd)
+// console.log("sum of Odd:"+sum_odd)
