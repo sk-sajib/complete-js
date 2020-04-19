1c9a237a77ae8ed77602f06a9868774149f8bab7
sk-sajib <sksajib0000@gmail.com>
2020-04-20 01:47:15 +0600

    Method.js Doen

---
+++ chapter-six/methods.js
@@ -0,0 +1,25 @@
+// var arr = [ 23, 44, 56, 78,98 ]
+// arr2 = [10, 34, 56, 21]
+
+//console.log(arr.join( ' / '))
+
+//console.log(arr.fill( false ))
+
+// var arr3 = arr.concat(arr2)
+// console.log(arr3)
+
+//console.log(Array.isArray(arr))
+
+arr2 = [10, 34, 56, 21]
+
+arr3 = Array.from(arr2)
+ arr3[0] = 11
+console.log(arr2,arr3)
+
+// var a = [ 1, 2]
+// var b = a
+
+// b[0] = 5
+
+// console.log(a,b)
+
