e5cf15890191cf85bfde73cc691d591307f0fde0
sk-sajib <sksajib0000@gmail.com>
2020-04-19 23:48:56 +0600

    search algorithm done

---
+++ chapter-six/search.js
@@ -0,0 +1,36 @@
+
+// var arr = [1 , 23, 45, 56, 67, 78]
+
+// var find = 11
+// var isFound = false
+
+// for (var i =0;i<arr.length;i++){
+//     if(arr[i] == find){
+//         console.log("Yes data found at index" + i)
+//         isFound = true
+//         break
+//     } 
+// }
+
+// if (!isFound){
+//     console.log("no data found")
+// }
+
+
+
+// var arr = [1, 3, 5, 6]
+// var found = 1
+// var empire
+
+// for ( var i =0; i< arr.length; i++){
+//     if (arr[i]== found){
+//         empire = 'Data found ' + i
+//         break
+//     } else{
+//         empire = 'data not found'
+//     }
+  
+// }
+
+// console.log(empire)
+
