37df90d82075aa28b99f47bb07d9b2ebbf3eafa9
sk-sajib <sksajib0000@gmail.com>
2020-04-20 01:08:54 +0600

    rever.js done

---
+++ chapter-six/reverse.js
@@ -0,0 +1,20 @@
+var arr = [ 1, 3, 4, 6, 9, 23,32]
+
+// for ( var i = 0; i<(arr.length/2);i++){
+//     var temp =arr[i]
+//     arr[i] = arr[arr.length-1 - i] 
+//     arr[arr.length-1 - i] = temp
+    
+// }
+
+// console.log(arr)
+
+// for ( var i =0; i<arr.length/2;i++){
+//     var temp = arr[i]
+//     arr[i] = arr[arr.length-1 - i]
+//     arr[arr.length-1 - i] = temp
+// }
+
+// console.log(arr)
+
+console.log(arr.reverse())
