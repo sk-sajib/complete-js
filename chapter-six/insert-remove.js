67635e2608be2b90b11e5fc138f6372a437c6a07
sk-sajib <sksajib0000@gmail.com>
2020-04-19 23:08:24 +0600

    inserRemove.js Done

--- chapter-six/insert-remove.js
+++ chapter-six/insert-remove.js
@@ -1,1 +1,17 @@
-arr[122]
+
+// Insert and Remove
+
+// var arr = [ 1, 2, 3, 4 , 5, 6, 7,8]
+
+// [3] = 9
+// arr.push(9) // push method add element/data at end
+// arr.unshift(9) //unshift methos add the element/data at the first 
+
+// arr.splice( 5, 1, 10,12 ) // Splice use for both add and remove (index no,how many wants to delete, add the data)
+
+// arr.pop() // pop method remove the last data
+// arr.shift() // shift method remove the first data
+
+// arr.splice( 3, 2,23, 44)
+
+// console.log(arr)
