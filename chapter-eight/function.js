780fea782d4844b12744b4acbaf2285c3cbab16f
sk-sajib <sksajib0000@gmail.com>
2020-04-20 22:01:21 +0600

    function

---
+++ chapter-eight/function.js
@@ -0,0 +1,29 @@
+// What is function 
+// A function holo amon akta system, jekhane choto choto program slove kore multiple time use kora hoy just function ta use kore
+
+
+function functionName(){
+    console.log("I am JavaScript developer")
+}
+
+function add(){
+    var a = 10
+    var b = 10
+    var c = a+b
+    console.log(c)
+}
+
+function sub (){
+    var a = 20
+    var b =24
+    console.log(a-b)
+}
+
+functionName()
+add()
+sub()
+
+for ( var i = 0;i<10;i++){
+    add()
+}
+
