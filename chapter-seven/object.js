3b547e7c35910cead523957b9dac563aa6a09fe3
sk-sajib <sksajib0000@gmail.com>
2020-04-20 13:29:29 +0600

    object.js Done

---
+++ chapter-seven/object.js
@@ -0,0 +1,43 @@
+// What is object
+
+// In JavaScript, an object is a standalone entity, with properties and type. Compare it with a cup, for example. A cup is an object, with properties. A cup has a color, a design, weight, a material it is made of, etc
+
+
+// var cup ={
+//     color: blue,
+//     weight: 2,
+//     madeWith: silver
+
+// }
+
+/// Object Literal vs Constructor
+
+
+var obj = {}
+// console.log(typeof obj)
+
+// obj.x =10
+// console.log(obj)
+
+
+// var man = {
+//     name: "sajb khan",
+//     profession: "student",
+//     age : '24'
+
+// }
+// man.age = "22"
+// console.log(man)
+
+// Object Constructor used in OOP JS
+
+var obj = Object()
+obj.a = 10
+
+console.log(obj)
+
+var obj2 = new Object()
+obj2.b = 11
+
+console.log(obj2)
+
